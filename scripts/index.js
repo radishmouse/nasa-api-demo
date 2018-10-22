53d1254a3e7dffe370608352e0b9ebbb67b07535
Chris Aquino <chris.aquino@gmail.com>
2018-10-22 11:24:35 -0400

    shows results of exploring the json data

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 console.log('boop beep boop');
+
+
+// ====================================== 
+// Data pathfinding experiments
+// ====================================== 
+
+// This finds the minimum estimated diameter
+// dummyData['near_earth_objects']['2015-09-08'][0]['estimated_diameter']["feet"]['estimated_diameter_min']
