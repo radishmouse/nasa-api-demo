0861eb96032458dd7c03322cb136e92db4cfe531
Chris Aquino <chris.aquino@gmail.com>
2018-10-22 12:19:38 -0400

    adds very simple error handling, providing data from localStorage

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,8 @@ console.log('boop beep boop');
 // dummyData['near_earth_objects']['2015-09-08'][0]['estimated_diameter']["feet"]['estimated_diameter_min']
 
 const URL = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${API_KEY}`;
+// "mangled" version for testing the .catch
+// const URL = `https://klahfkajsdhfkjashgdfjasdhgfjdsagfjasgdfjasg/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${API_KEY}`;
 
 // 1. expect to receive the data as an argument
 function cacheResults(asteroidData) {
@@ -24,14 +26,26 @@ function cacheResults(asteroidData) {
 }
 
 function pullResultsFromCache() {
+    // 1. Grab the string data from localStorage
+    let cachedResults = localStorage.getItem('asteroids');
+    // 2. Convert the string to JavaScript data (object or array, etc.)
+    let convertedResults = JSON.parse(cachedResults);
+    // 3. Return my data
+    return convertedResults;
+}
 
+function drawResultsToPage(asteroidData) {
+    console.log('i got the datazzz! woot!');
+    console.log(asteroidData);
 }
 
 function getAsteroidData() {
     fetch(URL) // fetch the data
         .then(r => r.json()) // convert to json
         .then(cacheResults)// cache the results
-    // catch any errors, returning cached info to next link in promise chain
+        .catch(pullResultsFromCache)// catch any errors, returning cached info to next link in promise chain
+        .then(drawResultsToPage)
+    
     // draw results to screen
 }
 
